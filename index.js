0ec1485bb49f79f0b8828d4d6bc5a75662e82971
awesomebunny <awesomebunny@bellsouth.net>
2024-09-09 14:30:45 -0400

    Exercise 3.1

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let phonebook = [
+    {
+        "id": "1",
+        "name": "Mario Mario",
+        "number": "(843) 727-8954",
+        "favorite": true
+    },
+    {
+        "id": "2",
+        "name": "Hazel-Rah",
+        "number": "(464) 919-7737",
+        "favorite": false
+    },
+    {
+        "id": "3",
+        "name": "Jason Funderburker",
+        "number": "(919) 747-2179",
+        "favorite": false
+    },
+    {
+        "id": "4",
+        "name": "Link Link",
+        "number": "(222) 222-2222",
+        "favorite": false
+    },
+];
+
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+    response.json(phonebook);
+});
+
+
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
